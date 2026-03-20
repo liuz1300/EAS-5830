5143902c827ce81cbdd99e5f4026ff83c8406921
liuz1300 <140762861+liuz1300@users.noreply.github.com>
2026-03-19 22:31:41 -0400

    Update signatures.py

--- signatures.py
+++ signatures.py
@@ -33,6 +33,7 @@ def verify(m, public_key, signed_message):
 
     # TODO verify the 'signed_message' is valid given the original message 'm' and the signers 'public_key'
     message = encode_defunct(text=m)  # Encode the message
+    signed_message = bytes.fromhex(signed_message[2:])
     signer = eth_account.Account.recover_message(message, signature=signed_message)  # Verify the message
     valid_signature = public_key.lower() == signer.lower()  # True if message verifies, False if message does not verify
 
