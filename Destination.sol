92aac13eb85fa7276d3fa7904cb60c431e25cb3a
liuz1300 <140762861+liuz1300@users.noreply.github.com>
2026-03-28 12:58:49 -0400

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -22,8 +22,30 @@ contract Destination is AccessControl {
         _grantRole(WARDEN_ROLE, admin);
     }
 
-	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		//YOUR CODE HERE
+	function createToken(
+        address _underlying_token,
+        string memory name,
+        string memory symbol
+    ) public onlyRole(CREATOR_ROLE) returns (address) {
+        require(_underlying_token != address(0), "Invalid underlying");
+        require(bridgeTokens[_underlying_token] == address(0), "Token already registered");
+
+        // Deploy BridgeToken with bridge contract as admin
+        BridgeToken token = new BridgeToken(_underlying_token, name, symbol, address(this));
+        bridgeTokens[_underlying_token] = address(token);
+
+        // Grant the bridge contract MINTER_ROLE
+        token.grantRole(MINTER_ROLE, address(this));
+
+        emit Creation(_underlying_token, address(token));
+        return address(token);
+    }
+
+    function wrap(
+        address _underlying_token,
+        address _recipient,
+        uint256 _amount
+    ) public onlyRole(WARDEN_ROLE) {
         require(bridgeTokens[_underlying_token] != address(0), "Token not registered");
         require(_recipient != address(0), "Invalid recipient");
         require(_amount > 0, "Amount must be > 0");
@@ -32,10 +54,13 @@ contract Destination is AccessControl {
         token.mint(_recipient, _amount);
 
         emit Wrap(_underlying_token, _recipient, _amount);
-	}
+    }
 
-	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
+    function unwrap(
+        address _wrapped_token,
+        address _recipient,
+        uint256 _amount
+    ) public {
         require(_wrapped_token != address(0), "Invalid token");
         require(_recipient != address(0), "Invalid recipient");
         require(_amount > 0, "Amount must be > 0");
@@ -43,31 +68,8 @@ contract Destination is AccessControl {
         BridgeToken token = BridgeToken(_wrapped_token);
         require(token.balanceOf(msg.sender) >= _amount, "Insufficient balance");
 
-        // Burns tokens from caller
         token.burnFrom(msg.sender, _amount);
 
         emit Unwrap(_wrapped_token, _recipient, _amount);
     }
-	}
-
-	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		//YOUR CODE HERE
-        require(_underlying_token != address(0), "Invalid underlying");
-        require(bridgeTokens[_underlying_token] == address(0), "Token already registered");
-
-        // Deploy the BridgeToken with the bridge contract as admin
-        BridgeToken token = new BridgeToken(_underlying_token, name, symbol, address(this));
-
-        // Store mapping
-        bridgeTokens[_underlying_token] = address(token);
-
-        // Grant the bridge contract the MINTER_ROLE so it can mint tokens
-        token.grantRole(token.MINTER_ROLE(), address(this));
-
-        emit Creation(_underlying_token, address(token));
-        return address(token);
-	}
-
 }
-
-
