7f7bcbd76a03aa339e30b02bf4ee77d62235574f
liuz1300 <140762861+liuz1300@users.noreply.github.com>
2026-03-28 11:30:51 -0400

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -159,9 +159,9 @@ def send_signed_msg(proof, random_leaf):
     signed_tx = w3.eth.account.sign_transaction(tx, private_key=acct.key)
 
     # Send transaction
-    tx_hash = signed_tx.hex()
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
 
-    return tx_hash
+    return tx_hash.hex()
 
 
 # Helper functions that do not need to be modified
