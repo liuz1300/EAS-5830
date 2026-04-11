02ce4d6e149d22dfa6fa6b65287a493b3a0bf438
liuz1300 <140762861+liuz1300@users.noreply.github.com>
2026-04-11 20:47:59 +0530

    Update listener.py

--- listener.py
+++ listener.py
@@ -49,7 +49,8 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
         print( f"Scanning block {start_block} on {chain}" )
     else:
         print( f"Scanning blocks {start_block} - {end_block} on {chain}" )
-    rows = []
+    
+	rows = []
     if end_block - start_block < 30:
         event_filter = contract.events.Deposit.create_filter(from_block=start_block,to_block=end_block,argument_filters=arg_filter)
         events = event_filter.get_all_entries()
@@ -90,6 +91,6 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
         else:
             df.to_csv(file_path, mode='w', header=True, index=False)
 
-        print(f"Saved {len(rows)} events to {eventfile}")
-    else:
-        print("No events found.")
+        	print(f"Saved {len(rows)} events to {eventfile}")
+   		else:
+        	print("No events found.")
